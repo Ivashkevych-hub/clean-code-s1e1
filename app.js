bc7dfe59a0c1e07911d41b5cce5a910d813b7734
Alexei Ivashkevych <ivashkevich1991@gmail.com>
2022-05-29 10:47:46 +0300

    refactor:rewrite CSS wihtout using tag selectors 3.4  in html-and-css.md

--- app.js
+++ app.js
@@ -37,14 +37,17 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className = 'checkbox';
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task input-text";
+    listItem.className="task-list-wrapper";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="button edit";
 
     deleteButton.className="button delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className='del-img';
     deleteButton.appendChild(deleteButtonImg);
 
 
